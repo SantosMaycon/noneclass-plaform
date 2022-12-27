cc3516cd0dcf893189b2cef29164ea1227983d9c
SantosMaycon <mayconalves.santos14@gmail.com>
2022-12-27 01:36:39 -0200

    chore: config animator and script of player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
   [SerializeField] private float speed;
+  [SerializeField] private float jumpForce;
+  [SerializeField] private int amountOfJump = 1;
   private Rigidbody2D rigidbody2d;
   private Animator animator;
+  private int totalOfJump;
   // Start is called before the first frame update
   void Start() {
     rigidbody2d = GetComponent<Rigidbody2D>();
     animator = GetComponent<Animator>();
+    totalOfJump = amountOfJump;
   }
 
   // Update is called once per frame
   void Update() {
     move();
+    jump();
   }
 
-  private void move () {
+  private void move() {
     float horizontal = Input.GetAxis("Horizontal");
 
     if (horizontal != 0f) {
@@ -29,5 +34,29 @@ public class PlayerController : MonoBehaviour {
 
     float velocity = horizontal * speed;
     rigidbody2d.velocity = new Vector2(velocity, rigidbody2d.velocity.y);
+  }
+
+  private void jump() {
+    animator.SetFloat("verticalVelocity", rigidbody2d.velocity.y);
+
+    if (Input.GetButtonDown("Jump") && amountOfJump > 0) {
+      rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
+      animator.SetBool("isOnTheFloor", false);
+      amountOfJump--;
+    }
+  }
+
+  private void OnCollisionEnter2D(Collision2D other) {
+    if (other.gameObject.CompareTag("Floor")) {
+      animator.SetBool("isOnTheFloor", true);
+      amountOfJump = totalOfJump;
+    }
+  }
+
+  private void OnCollisionExit2D(Collision2D other) {
+    if (other.gameObject.CompareTag("Floor")) {
+      animator.SetBool("isOnTheFloor", false);
+      Debug.Log("Is not on the floor");
+    }
   }
 }
