ff20d7f72975a49f0b4cff73b9038f35c68fcff8
SantosMaycon <mayconalves.santos14@gmail.com>
2022-12-28 01:15:42 -0200

    chore: add enemy game object

---
+++ Assets/Scripts/PigController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigController : MonoBehaviour {
+  [SerializeField] private float speed;
+  [SerializeField] private float timeToWalk;
+  [SerializeField] private LayerMask levelLayer;
+  private Rigidbody2D rigidbody2d;
+  private Animator animator;
+  private BoxCollider2D boxCollider2d;
+  // Start is called before the first frame update
+  void Start() {
+    rigidbody2d = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+    boxCollider2d = GetComponent<BoxCollider2D>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    move();
+  }
+
+  private void FixedUpdate() {
+    if (hitTheWall()) {
+      rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x * -1f, rigidbody2d.velocity.y);
+      transform.localScale = new Vector3(Mathf.Sign(rigidbody2d.velocity.x) * -1, 1f, 1f);  
+    }
+  }
+
+  private bool hitTheWall() {
+    return Physics2D.Raycast(boxCollider2d.bounds.center, new Vector2(Mathf.Sign(rigidbody2d.velocity.x), 0f), 0.5f, levelLayer);
+  }
+
+  private void move() {
+    if (timeToWalk <= 0) {
+      var direction = Random.Range(-1, 2);
+      rigidbody2d.velocity = new Vector2(speed * direction, rigidbody2d.velocity.y);
+      animator.SetBool("isMove", rigidbody2d.velocity.x != 0);
+      
+      if (rigidbody2d.velocity.x != 0) {
+        transform.localScale = new Vector3(Mathf.Sign(rigidbody2d.velocity.x) * -1, 1f, 1f);  
+      }
+
+      timeToWalk = Random.Range(2f, 8f);
+    } else {
+      timeToWalk -= Time.deltaTime;
+    }
+  } 
+}
